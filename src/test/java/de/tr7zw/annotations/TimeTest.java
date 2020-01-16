66043a0a0b56ecc1f9a919dbe4c48f9b49138008
tr7zw <tr7zw@live.de>
2020-01-16 15:19:20 +0100

    Do something with the version so the compiler can't optimize it away, give it 2ms max

--- src/test/java/de/tr7zw/annotations/TimeTest.java
+++ src/test/java/de/tr7zw/annotations/TimeTest.java
@@ -4,6 +4,7 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -20,8 +21,9 @@ public class TimeTest {
 		long time = System.currentTimeMillis();
 		int version = FAUtil.getAnnotation(this::readValue, VersionInfo.class).version(); // 0ms
 		time = System.currentTimeMillis() - time;
+		System.out.println(version);
 		System.out.println(time);
-		assertEquals(0, time);
+		assertTrue(time < 2);
 	}
 
 	@Retention(RUNTIME)
